b34713613638c03edbf5cd2890891f0851e6ba53
Kirthika K <kirthika.kalahasthi@anywhere.co>
2026-07-21 15:01:35 +0530

    Initial Commit

---
+++ Richtext/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Richtext
+//
+//  Created by Kirthika Kalahasthi on 21/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Richtext/RichtextApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichtextApp.swift
+//  Richtext
+//
+//  Created by Kirthika Kalahasthi on 21/07/26.
+//
+
+import SwiftUI
+
+@main
+struct RichtextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
